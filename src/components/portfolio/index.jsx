6425772f1da12ac1d29fa3b722a4656376e830e2
MOHAMMAD SAIDUL ISLAM <isaidul92@gmail.com>
2023-07-25 08:29:27 +0600

    portfolio page added
    portfolio page added

--- src/components/portfolio/index.jsx
+++ src/components/portfolio/index.jsx
@@ -1,28 +1,27 @@
-import Link from "next/link"
-import { Col } from "react-bootstrap"
+import Link from "next/link";
+import { Col } from "react-bootstrap";
+import DOMPurify from "dompurify";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+const Portfolioitem = ({ data, slug }) => {
+  return (
+    <Col xs={12} sm={6} lg={4} className="ltn__gallery-item filter_category_3">
+      <div className="ltn__gallery-item-inner">
+        <div className="ltn__gallery-item-img">
+          <Link href={`/img/gallery/${data.thumbImage}`}>
+            <img src={`/img/gallery/${data.thumbImage}`} alt="Image" />
+            <span className="ltn__gallery-action-icon">{<FaArrowRight />}</span>
+            {/* <span className="ltn__gallery-action-icon" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(data.icon)}}></span> */}
+          </Link>
+        </div>
+        <div className="ltn__gallery-item-info">
+          <h4>
+            <Link href={`${slug}`}>{data.title}</Link>
+          </h4>
+          <p>{data.designation}</p>
+        </div>
+      </div>
+    </Col>
+  );
+};
 
-const Portfolioitem = ({ data }) => {
-    return (
-
-        <Col xs={12} sm={6} lg={4} className="ltn__gallery-item filter_category_3">
-            <div className="ltn__gallery-item-inner">
-                <div className="ltn__gallery-item-img">
-                    <Link href={`/img/gallery/${data.thumbImage}`}>
-                        <img src={`/img/gallery/${data.thumbImage}`} alt="Image" />
-                        <span className="ltn__gallery-action-icon">
-                            {data.icon}
-                        </span>
-                    </Link>
-                </div>
-                <div className="ltn__gallery-item-info">
-                    <h4><Link href={`${slug}`}>{data.title}</Link></h4>
-                    <p>{data.designation}</p>
-                </div>
-            </div>
-        </Col>
-
-
-    )
-}
-
-export default Portfolioitem
+export default Portfolioitem;
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -2,15 +2,53 @@ import { LayoutOne } from "@/layouts";
 import { Container, Row, Col } from "react-bootstrap";
 import { getProducts, productSlug } from "@/lib/product";
 import ShopBreadCrumb from "@/components/breadCrumbs/shop";
+import TitleSection from "@/components/titleSection";
 import BlogItem from "@/components/blog";
 import blogData from "@/data/blog";
-import CallToAction from "@/components/callToAction";
-import portfolioData from '@/data/portfolio';
+import CallToActionstyleTwo from "@/components/callToAction/callToActionstyleTwo";
+import portfolioData from "@/data/portfolio";
 import Portfolioitem from "@/components/portfolio";
+import Slider from "react-slick";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import CallToAction from "@/components/callToAction";
+import brandLogoData from "@/data/brand-logo";
 
 function Portfolio() {
   const portfolios = getProducts(portfolioData, "fashion", "featured", 6);
-
+  const blogSettings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: (
+      <a>
+        <FaArrowLeft />
+      </a>
+    ),
+    nextArrow: (
+      <a>
+        <FaArrowRight />
+      </a>
+    ),
+  };
+  const LogoSettings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow: (
+      <a>
+        <FaArrowLeft />
+      </a>
+    ),
+    nextArrow: (
+      <a>
+        <FaArrowRight />
+      </a>
+    ),
+  };
 
   return (
     <>
@@ -21,26 +59,84 @@ function Portfolio() {
           currentSlug="Portfolio"
         />
 
+        {/*  <!-- Gallery area start -->  */}
+        <div className="ltn__gallery-area mb-120">
+          <Container>
+            <Row className="ltn__gallery-active ltn__gallery-style-2 ltn__gallery-info-hide---">
+              {portfolios.map((data, key) => {
+                const slug = productSlug(data.title);
+
+                return (
+                  <Portfolioitem
+                    key={key}
+                    baseUrl="portfolio"
+                    data={data}
+                    slug={slug}
+                  />
+                );
+              })}
+            </Row>
+            <div className="btn-wrapper text-center">
+              <button className="btn btn-transparent btn-effect-3 btn-border">
+                LOAD MORE +
+              </button>
+            </div>
+          </Container>
+        </div>
+        {/*  <!-- Gallery area end --> */}
+
+        <CallToActionstyleTwo />
 
         {/* <!-- BLOG AREA START (blog-3) -->  */}
-        <div className="ltn__blog-area pb-70">
+        <div className="ltn__blog-area pt-120 pb-70">
           <Container>
             <Row>
               <Col lg={12}>
-                {portfolios.map((data, key) => {
-                  const slug = productSlug(data.title);
-                  return (
-                    <>
-                      <Portfolioitem key={key} baseUrl="portfolio" data={data} slug={slug} />
-                    </>
-                  );
-                })}
+                <TitleSection
+                  titleSectionData={{
+                    subTitle: "News & Blogs",
+                    title: "Leatest News Feeds",
+                  }}
+                />
               </Col>
             </Row>
-
+            <Slider
+              {...blogSettings}
+              className="ltn__blog-slider-one-active slick-arrow-1 ltn__blog-item-3-normal"
+            >
+              {blogData.map((data, key) => {
+                const slug = productSlug(data.title);
+                return (
+                  <BlogItem key={key} baseUrl="blog" data={data} slug={slug} />
+                );
+              })}
+            </Slider>
           </Container>
         </div>
         {/* <!-- BLOG AREA END --> */}
+
+        {/* <!-- BRAND LOGO AREA START --> */}
+        <div className="ltn__brand-logo-area ltn__brand-logo-1 pt-80--- pb-110 plr--9">
+          <Container fluid>
+            <Row className="ltn__brand-logo-active">
+              <Col xs={12}>
+                <Slider {...LogoSettings}>
+                  {brandLogoData.map((logo, key) => {
+                    return (
+                      <div key={key} className="ltn__brand-logo-item">
+                        <img
+                          src={`/img/brand-logo/${logo.image}`}
+                          alt="Brand Logo"
+                        />
+                      </div>
+                    );
+                  })}
+                </Slider>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        {/* <!-- BRAND LOGO AREA END --> */}
 
         <div className="ltn__call-to-action-area call-to-action-6 before-bg-bottom">
           <Container>
